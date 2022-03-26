d4967130b12ae54518790bd12e206f352e417e0c
ScottyP666 <63470831+ScottyP666@users.noreply.github.com>
2022-03-25 19:41:20 -0700

    l

--- Assignment11/src/main/java/com/codercampus/Assignment11/Assignment11Application.java
+++ Assignment11/src/main/java/com/codercampus/Assignment11/Assignment11Application.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class Assignment11Application {
